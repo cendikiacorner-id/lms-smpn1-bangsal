138b9d4b502c3362b5ae35bbd213486727b13a09
cendikiacorner-id <yudisiswanto45@guru.smp.belajar.id>
2026-08-12 00:28:33 +0700

    Deploy LMS SMPN 1 Bangsal Cloud v1.0

---
+++ config.js
@@ -0,0 +1,6 @@
+/* Tempel URL deployment Google Apps Script yang berakhiran /exec. */
+window.LMS_CONFIG = Object.freeze({
+  API_URL: 'https://script.google.com/macros/s/AKfycbyM8XbRX8QuzPIwiKuiPPDOcXMhk-NAIvV8PUfiL86DKnLGmo0EB_xEkb77QVSzgpJtMA/exec',
+  REQUEST_TIMEOUT_MS: 45000,
+  UPLOAD_TIMEOUT_MS: 120000
+});
